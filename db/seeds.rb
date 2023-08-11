c34fd1d6a7464c37396f755a62ec5694a55952b4
mansaridev <developer.mansari@gmail.com>
2023-08-11 10:53:28 -0700

    Updated seed file for data population

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create 5 teams
+team_names = (1..5).map { |i| { name: Faker::Team.name } }
+teams = Team.create(team_names)
+
+# Create 20 members with random values for each field
+20.times do
+  team = teams.sample
+
+  Member.create(
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    team: team,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: Faker::Address.country
+  )
+end
+
+# Create 5 projects
+5.times do
+  Project.create(name: Faker::App.name)
+end
+
+# Assign members to the projects
+Project.all.each_with_index do |project, index|
+  project.members << Member.all.limit(2).offset(index*2)
+end
